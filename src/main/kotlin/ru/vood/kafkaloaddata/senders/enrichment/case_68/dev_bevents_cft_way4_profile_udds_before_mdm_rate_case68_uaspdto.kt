3eb36502f9d1bae419bdc81f0448653558a2219f
vood <balitskY_igor@list.ru>
2022-12-28 18:38:39 +0300

    s

--- src/main/kotlin/ru/vood/kafkaloaddata/senders/enrichment/case_68/dev_bevents_cft_way4_profile_udds_before_mdm_rate_case68_uaspdto.kt
+++ src/main/kotlin/ru/vood/kafkaloaddata/senders/enrichment/case_68/dev_bevents_cft_way4_profile_udds_before_mdm_rate_case68_uaspdto.kt
@@ -1,4 +1,4 @@
-package ru.vood.kafkaloaddata.senders.filter
+package ru.vood.kafkaloaddata.senders.enrichment.case_68
 
 import kotlinx.serialization.encodeToString
 import org.slf4j.Logger
@@ -9,37 +9,39 @@ import ru.vood.kafkaloaddata.dto.SerialisationConst
 import ru.vood.kafkaloaddata.dto.SomeDto
 import ru.vood.kafkaloaddata.producer.MessageProducerInterface
 import ru.vood.kafkaloaddata.senders.ToTopicLoader
+import ru.vood.kafkaloaddata.senders.enrichment.way4Flat.dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdtoFlat
 import java.util.*
 
 @Service
-class dev_ivr__uasp_realtime__business_rules__uaspdto(
+class dev_bevents_cft_way4_profile_udds_before_mdm_rate_case68_uaspdto(
     override val messageProducer: MessageProducerInterface<String, String>,
     override val countProperties: CountProperties
 ) : ToTopicLoader<SomeDto> {
 
     override val topicName: String
-        get() = "dev_bevents__realtime__aggregate_first_salary__uaspdto"
+        get() = "dev_bevents_cft_way4_profile_udds_before_mdm_rate_case68_uaspdto"
 
-    override val logger: Logger = LoggerFactory.getLogger(dev_ivr__uasp_realtime__business_rules__uaspdto::class.java)
+    override val logger: Logger = LoggerFactory.getLogger(
+        dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdtoFlat::class.java
+    )
 
-    override val generateFun: (Long) -> SomeDto = { id ->
-        SomeDto(
-            "global_id_$id",
-            mutableMapOf(),
-            mutableMapOf(),
-            mutableMapOf(),
-            mutableMapOf(),
-            mutableMapOf(),
-            mutableMapOf("system-classification" to "asd"),
-            mutableMapOf("is_mortgage" to true),
-            id.toString(),
-            Calendar.getInstance().timeInMillis,
-
-            )
-    }
     override val timeOut: Optional<Int>
         get() = Optional.empty()
 
+    override val generateFun: (Long) -> SomeDto = { id ->
+        SomeDto(
+            id.toString(),
+            mutableMapOf(),
+            mutableMapOf(),
+            mutableMapOf(),
+            mutableMapOf(),
+            mutableMapOf(),
+            mutableMapOf("global_id" to "global_id_$id"),
+            mutableMapOf(),
+            id.toString(),
+            Calendar.getInstance().timeInMillis,
+        )
+    }
 
     override fun json(t: SomeDto): String = SerialisationConst.customJson.encodeToString(t)
 
